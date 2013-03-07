8a45a5b72254ba94303bfff14ab02d2d44ee7e1c
Sean Lee <weilonge@gmail.com>
2013-03-07 17:33:56 +0800

    Add CARD_SPEC to draw a card and refactor the draw function.

--- js/take6.js
+++ js/take6.js
@@ -1,38 +1,25 @@
 $(function (){
-  for(var i = 52; i<56; i++){
-    createCard("t" + i.toString(), i);
-  }
-
-  function callback() {
-    setTimeout(function() {
-      $( "#t55" ).hide().fadeIn();
-    }, 1000 );
-  };
-
-  $( "#button" ).click(function() {
-    $( "#t55" ).effect( 'blind', null, 500, callback );
-    return false;
-  });
+  createTable("cardTable");
 });
 
-function createCard(id, value) {
+function createTable(id) {
   var CARD_H = 210;
   var CARD_W = 150;
   var CARD_SCALE = 0.75;
 
-  var card$ = $('#' + id);
-  card$.draggable();
-  card$.height( CARD_H * CARD_SCALE );
-  card$.width( CARD_W * CARD_SCALE );
-
   var stage = new Kinetic.Stage({
-    width: CARD_W * CARD_SCALE + 3,
-    height: CARD_H * CARD_SCALE + 3,
+    width: 500,
+    height: 500,
     scale: CARD_SCALE,
     container: id,
     id: 'can_' + id
   });
-  stage.add(drawCard(value, CARD_W, CARD_H));
+  var scoreList = [1, 2, 5, 10, 11, 55];
+  for(var i = 0; i<6; i++){
+    var ccc = drawCard(scoreList[i], CARD_W, CARD_H,  i*50 + 3,  i*80 + 3);
+    ccc.setDraggable(true);
+    stage.add( ccc );
+  }
 }
 
 function drawRect(w, h) {
@@ -47,55 +34,75 @@ function drawRect(w, h) {
   return rr;
 }
 
-function drawCard(number, w, h) {
+function drawCard(number, w, h, x, y) {
   var card = new Kinetic.Layer({
-    x: 0,
-    y: 0,
+    x: x,
+    y: y,
     scale: 1
   });
   card.add(drawRect(w, h));
   if( 0 == (number % 5) && 0 == ( number % 11) ) {
-    card.add(drawBackground('purple'));
-
-    card.add(drawLogo('red', 75, 100, 1.5, 0));
-
-    card.add(drawLogo('red',  45, 10, 0.15, 0));
-    card.add(drawLogo('red',  55, 25, 0.15, 0));
-    card.add(drawLogo('red',  65, 10, 0.15, 0));
-    card.add(drawLogo('red',  75, 25, 0.15, 0));
-    card.add(drawLogo('red',  85, 10, 0.15, 0));
-    card.add(drawLogo('red',  95, 25, 0.15, 0));
-    card.add(drawLogo('red', 105, 10, 0.15, 0));
-
-    card.add(drawLogo('red',  45, 210 - 10, 0.15, 180));
-    card.add(drawLogo('red',  55, 210 - 25, 0.15, 180));
-    card.add(drawLogo('red',  65, 210 - 10, 0.15, 180));
-    card.add(drawLogo('red',  75, 210 - 25, 0.15, 180));
-    card.add(drawLogo('red',  85, 210 - 10, 0.15, 180));
-    card.add(drawLogo('red',  95, 210 - 25, 0.15, 180));
-    card.add(drawLogo('red', 105, 210 - 10, 0.15, 180));
+    drawRawCard(card, number, 7);
+  } else if( 0 == (number % 11) ) {
+    drawRawCard(card, number, 5);
+  } else if( 0 == (number % 10) ) {
+    drawRawCard(card, number, 3);
+  } else if( 0 == (number % 5) ) {
+    drawRawCard(card, number, 2);
   } else {
-    card.add(drawLogo('purple', 75, 100, 1.5, 0));    
+    drawRawCard(card, number, 1);
   }
-  card.add(drawText(number, 75, 105, 1.6, 0, '#EEEE00', '#222222'));
-
-  card.add(drawText(number,  17,  20, 0.5, 0, '#EEEE00', '#222222'));
-  card.add(drawText(number,  17, 190, 0.5, 180, '#EEEE00', '#222222'));
-  card.add(drawText(number, 133,  20, 0.5, 0, '#EEEE00', '#222222'));
-  card.add(drawText(number, 133, 190, 0.5, 180, '#EEEE00', '#222222'));
-
   return card;
 }
 
-function drawLogo(color, x, y, scale, deg) {
+function drawRawCard(card, number, score) {
+  card.add(drawBackground(
+      CARD_SPEC[score].BG.COLOR,
+      CARD_SPEC[score].BG.DATA
+  ));
+
+  card.add(drawLogo(
+    CARD_SPEC[score].LOGO.COLOR,
+    CARD_SPEC[score].LOGO.SCALE,
+    CARD_SPEC[score].LOGO.POS[0]
+  ));
+
+  for(var para in CARD_SPEC[score].SCORES.DATA ){
+    card.add(drawLogo(
+      CARD_SPEC[score].SCORES.COLOR,
+      CARD_SPEC[score].SCORES.SCALE,
+      CARD_SPEC[score].SCORES.DATA[para]
+    ));
+  }
+
+  card.add(drawText(
+    number,
+    CARD_SPEC[score].NUMS.LARGE.SCALE,
+    CARD_SPEC[score].NUMS.LARGE.COLOR_T,
+    CARD_SPEC[score].NUMS.LARGE.COLOR_S,
+    CARD_SPEC[score].NUMS.LARGE.POS[0]
+  ));
+
+  for(var para in CARD_SPEC[score].NUMS.SMALL.POS ){
+    card.add(drawText(
+      number,
+      CARD_SPEC[score].NUMS.SMALL.SCALE,
+      CARD_SPEC[score].NUMS.SMALL.COLOR_T,
+      CARD_SPEC[score].NUMS.SMALL.COLOR_S,
+      CARD_SPEC[score].NUMS.SMALL.POS[para]
+    ));
+  }
+}
+
+function drawLogo(color, scale, pos) {
   var TAKE6SVGDATA = 'm 35.9487,978.1695 c 0.52367,-4.0812 -0.32729,-5.5952 0.19639,-9.6763 -4.5821,-2.1064 -8.3787,-4.8052 -12.961,-6.9116 -0.98188,4.147 0.98187,9.0839 0,13.231 2.8802,2.5013 3.993,3.423 6.8732,5.9242 -6.4804,1.6456 -9.8188,3.4888 -16.299,5.1344 -0.45234,-10.207 4.5821,-20.8 7.0695,-33.373 -9.2781,13.991 -21.739,39.892 -20.619,41.865 0.66614,1.1741 18.787,2.4355 33.188,2.5673 -4.6476,5.1345 -9.2951,10.269 -13.943,15.403 5.0403,6.4512 10.081,12.902 15.121,19.352 -5.6294,2.1065 -11.259,4.2123 -16.888,6.3188 2.8802,3.0275 5.7604,6.0557 8.6405,9.0831 2.6183,-0.5264 5.2367,-1.0523 7.855,-1.579 0.13092,2.0404 0.26184,4.0812 0.39275,6.1215 3.2075,0.1973 6.2185,0.3951 9.424485,0.6085 3.205985,-0.1811 6.216985,-0.3789 9.424485,-0.5762 0.13091,-2.0403 0.26184,-4.0811 0.39275,-6.1215 2.6183,0.5267 5.2366,1.0526 7.855,1.579 2.8801,-3.0274 5.7603,-6.0556 8.6405,-9.0831 -5.629,-2.1065 -11.2586,-4.2123 -16.888,-6.3188 5.04,-6.45 10.0807,-12.9008 15.121,-19.352 -4.6479,-5.134 -9.2954,-10.2685 -13.943,-15.403 14.401,-0.1318 32.52187,-1.3932 33.188,-2.5673 1.12,-1.973 -11.3409,-27.874 -20.619,-41.865 2.4874,12.573 7.52184,23.166 7.0695,33.373 -6.4802,-1.6456 -9.8186,-3.4888 -16.299,-5.1344 2.8802,-2.5012 3.993,-3.4229 6.8732,-5.9242 -0.98188,-4.1471 0.98188,-9.084 0,-13.231 -4.5823,2.1064 -8.3789,4.8052 -12.961,6.9116 0.52367,4.0811 -0.32729,5.5951 0.19638,9.6763 -4.2548,-2.633 -3.7965,-1.3165 -8.05082,-3.96565 -4.25432,2.61685 -3.79602,1.30035 -8.05082,3.93335 z';
   var TAKE6SVGWIDTH = 88;
   var TAKE6SVGHEIGHT = 100;
 
   var take6 = new Kinetic.Path({
     data: TAKE6SVGDATA,
-    x: x,
-    y: y,
+    x: pos[0],
+    y: pos[1],
     width: TAKE6SVGWIDTH,
     height: TAKE6SVGHEIGHT,
     offset: {
@@ -105,23 +112,23 @@ function drawLogo(color, x, y, scale, deg) {
     scale: scale,
     fill: color
   });
-  take6.setRotationDeg(deg);
+  take6.setRotationDeg(pos[2]);
   return take6;
 }
 
-function drawBackground(color) {
+function drawBackground(color, data) {
   var polyBackground = new Kinetic.Polygon({
-    points: [15, 15, 135, 20, 135, 195, 20, 190],
+    points: data,
     fill: color,
     strokeWidth: 0
   });
   return polyBackground;
 }
 
-function drawText(number, x, y, scale, deg, fill, stroke) {
+function drawText(number, scale, fill, stroke, pos) {
   var text = new Kinetic.Text({
-    x: x,
-    y: y,
+    x: pos[0],
+    y: pos[1],
     width: 60,
     height: 60,
     scale: scale,
@@ -137,6 +144,164 @@ function drawText(number, x, y, scale, deg, fill, stroke) {
     },
     text: number.toString()
   });
-  text.setRotationDeg(deg);
+  text.setRotationDeg(pos[2]);
   return text;
 }
+
+var CARD_SPEC = {
+  1: {
+    BG: {
+      COLOR: 'white',
+      DATA: [0, 0]
+    },
+    NUMS: {
+      LARGE: {
+        SCALE: 1.6,
+        COLOR_T: 'white', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [75, 105, 0] ]
+      },
+      SMALL: {
+        SCALE: 0.5,
+        COLOR_T: 'black', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [17, 20, 0], [17, 190, 180], [133, 20, 0], [133, 190, 180] ]
+      }
+    },
+    LOGO: {
+      COLOR: 'purple',
+      SCALE: 1.5,
+      POS: [ [75, 100, 0] ]
+    },
+    SCORES: {
+      COLOR: 'black',
+      SCALE: 0.15,
+      DATA: [
+        [75, 10, 0],
+        [75, 210 - 10, 180]
+      ]
+    }
+  },
+  2: {
+    BG: {
+      COLOR: '#AAAAFF',
+      DATA: [15, 15, 135, 20, 135, 195, 20, 190]
+    },
+    NUMS: {
+      LARGE: {
+        SCALE: 1.6,
+        COLOR_T: '#EEEE00', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [75, 105, 0] ]
+      },
+      SMALL: {
+        SCALE: 0.5,
+        COLOR_T: 'blue', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [17, 20, 0], [17, 190, 180], [133, 20, 0], [133, 190, 180] ]
+      }
+    },
+    LOGO: {
+      COLOR: 'blue',
+      SCALE: 1.5,
+      POS: [ [75, 100, 0] ]
+    },
+    SCORES: {
+      COLOR: 'blue',
+      SCALE: 0.15,
+      DATA: [
+        [65, 10, 0], [85, 10, 0],
+        [65, 210 - 10, 180], [85, 210 - 10, 180]
+      ]
+    }
+  },
+  3: {
+    BG: {
+      COLOR: 'orange',
+      DATA: [15, 15, 135, 20, 135, 195, 20, 190]
+    },
+    NUMS: {
+      LARGE: {
+        SCALE: 1.6,
+        COLOR_T: '#AAAAFF', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [75, 105, 0] ]
+      },
+      SMALL: {
+        SCALE: 0.5,
+        COLOR_T: 'red', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [17, 20, 0], [17, 190, 180], [133, 20, 0], [133, 190, 180] ]
+      }
+    },
+    LOGO: {
+      COLOR: 'red',
+      SCALE: 1.5,
+      POS: [ [75, 100, 0] ]
+    },
+    SCORES: {
+      COLOR: 'purple',
+      SCALE: 0.15,
+      DATA: [
+        [55, 10, 0], [75, 10, 0], [95, 10, 0],
+        [55, 210 - 10, 180], [75, 210 - 10, 180], [95, 210 - 10, 180]
+      ]
+    }
+  },
+  5: {
+    BG: {
+      COLOR: 'red',
+      DATA: [15, 15, 135, 20, 135, 195, 20, 190]
+    },
+    NUMS: {
+      LARGE: {
+        SCALE: 1.6,
+        COLOR_T: '#EEEE00', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [75, 105, 0] ]
+      },
+      SMALL: {
+        SCALE: 0.5,
+        COLOR_T: '#EEEE00', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [17, 20, 0], [17, 190, 180], [133, 20, 0], [133, 190, 180] ]
+      }
+    },
+    LOGO: {
+      COLOR: 'blue',
+      SCALE: 1.5,
+      POS: [ [75, 100, 0] ]
+    },
+    SCORES: {
+      COLOR: 'purple',
+      SCALE: 0.15,
+      DATA: [
+        [55, 10, 0], [65, 25, 0], [75, 10, 0], [85, 25, 0], [95, 10, 0],
+        [55, 210 - 10, 180], [65, 210 - 25, 0], [75, 210 - 10, 180], [85, 210 - 25, 0], [95, 210 - 10, 180]
+      ]
+    }
+  },
+  7: {
+    BG: {
+      COLOR: 'purple',
+      DATA: [15, 15, 135, 20, 135, 195, 20, 190]
+    },
+    NUMS: {
+      LARGE: {
+        SCALE: 1.6,
+        COLOR_T: '#EEEE00', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [75, 105, 0] ]
+      },
+      SMALL: {
+        SCALE: 0.5,
+        COLOR_T: '#EEEE00', COLOR_S: '#222222', COLOR_B: null,
+        POS: [ [17, 20, 0], [17, 190, 180], [133, 20, 0], [133, 190, 180] ]
+      }
+    },
+    LOGO: {
+      COLOR: 'red',
+      SCALE: 1.5,
+      POS: [ [75, 100, 0] ]
+    },
+    SCORES: {
+      COLOR: 'red',
+      SCALE: 0.15,
+      DATA: [
+        [45, 10, 0], [55, 25, 0], [65, 10, 0], [75, 25, 0], [85, 10, 0], [95, 25, 0], [105, 10, 0],
+        [45, 210 - 10, 180], [55, 210 - 25, 180], [65, 210 - 10, 180], [75, 210 - 25, 180], [85, 210 - 10, 180], [95, 210 - 25, 180], [105, 210 - 10, 180]
+      ]
+    }
+  }
+};
